23fc3b7eb46e6312b4e4614626dab9f284faa6db
tomuichro <sarahmendesandrade1@gmail.com>
2025-06-18 19:02:00 -0300

    upload react project

---
+++ site-react/app/src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+const Header = () =>{
+    return(
+        <header>
+            <h1>Aprendendo React.JS🚀</h1>
+            <p>Desenvolva suas habilidades de forma moderna e prática</p>
+        </header>
+    )
+}
+export default Header
